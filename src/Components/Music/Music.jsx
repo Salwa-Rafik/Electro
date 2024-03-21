d4bf44d611640920e5195c6760eb3308c2d9f155
Salwa Rafik <salwara37@gmail.com>
2024-03-22 01:34:23 +0200

    updates

--- src/Components/Music/Music.jsx
+++ src/Components/Music/Music.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import Slider from "react-slick";
+import { MdAddShoppingCart } from "react-icons/md";
+import { FaCodeCompare } from "react-icons/fa6";
+import { FaRegHeart } from "react-icons/fa";
 function Music() {
     const [products, setProducts] = useState([]);
     const [currentSlide, setCurrentSlide] = React.useState(0);
+    const [isHovered, setIsHovered] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -10,6 +14,7 @@ function Music() {
                 const response = await fetch("https://fakestoreapi.com/products/");
                 const data = await response.json();
                 setProducts(data);
+                setIsHovered(new Array(data.length).fill(false));
             } catch (error) {
                 console.error("Error fetching data:", error);
             }
@@ -57,7 +62,13 @@ function Music() {
         const words = title.split(" ");
         return words.slice(0, 3).join(" ");
     };
-
+    const handleMouseEnter = (index) => {
+        setIsHovered((prevState) => prevState.map((state, i) => i === index));
+      };
+    
+      const handleMouseLeave = () => {
+        setIsHovered((prevState) => prevState.map(() => false));
+      };
     return (
         <div className='container mx-auto px-16'>
             <div className="flex justify-between border-b border-gray-300 mt-8 mb-8">
@@ -65,16 +76,42 @@ function Music() {
             </div>
             <div className="slider-container">
                 <Slider {...settings}>
-                    {products.map((product) => (
+                    {products.map((product,index) => (
                         <div key={product.id}>
-                            <div className="flex justify-between bg-white h-40 m-0.5 ">
+                            <div className="flex justify-between bg-white h-40 m-0.5 " onMouseEnter={() => handleMouseEnter(index)}
+                onMouseLeave={handleMouseLeave}>
                                 <div>
                                     <img src={product.image} alt={product.title} className="h-20 w-auto m-8 pr-8" />
                                 </div>
                                 <div className="p-4">
                                     <h3 className="text-sm text-gray-500">{product.category}</h3>
                                     <h4 className="text-md font-bold text-blue-600">{truncateTitle(product.title)}</h4>
-                                    <p className="text-gray-700">${product.price}</p>
+                                    <div className="flex justify-between mt-4 text-lg">
+                    <div className="text-gray-600">
+                      <p>${product.price}</p>
+                    </div>
+                    <div
+                      className={`${
+                        isHovered[index] ? "bg-yellow-400" : "bg-gray-200"
+                      } text-center text-lg ml-8 p-2 rounded-full text-white`}
+                    >
+                      <MdAddShoppingCart />
+                    </div>
+                  </div>
+                  <div
+                    className={`${
+                      isHovered[index] ? "flex justify-between" : "hidden"
+                    } 'flex justify-between mt-4 text-gray-300 border-t border-gray-300'`}
+                  >
+                    <div className="flex items-center">
+                      <span className="ml-2">Wishlist</span>
+                      <FaRegHeart />
+                    </div>
+                    <div className="flex items-center">
+                      <span className="ml-2">Compare</span>
+                      <FaCodeCompare />
+                    </div>
+                  </div>
                                 </div>
                             </div>
                         </div>
--- src/Components/Telivision/Telivision.jsx
+++ src/Components/Telivision/Telivision.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
+import { MdAddShoppingCart } from "react-icons/md";
+import { FaCodeCompare } from "react-icons/fa6";
+import { FaRegHeart } from "react-icons/fa";
 
 function Telivision() {
   const [products, setProducts] = useState([]);
   const [currentSlide, setCurrentSlide] = React.useState(0);
+  const [isHovered, setIsHovered] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -13,6 +17,7 @@ function Telivision() {
         );
         const data = await response.json();
         setProducts(data);
+        setIsHovered(new Array(data.length).fill(false));
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -61,7 +66,13 @@ function Telivision() {
     const words = title.split(" ");
     return words.slice(0, 3).join(" ");
   };
+  const handleMouseEnter = (index) => {
+    setIsHovered((prevState) => prevState.map((state, i) => i === index));
+  };
 
+  const handleMouseLeave = () => {
+    setIsHovered((prevState) => prevState.map(() => false));
+  };
   return (
     <div
       className="bg-cover bg-center lg:h-[550px]  h-[320px] mt-8"
@@ -87,27 +98,54 @@ function Telivision() {
 
           <div className="slider-container lg:col-span-1 col-span-2 ">
             <Slider {...settings}>
-              {products.map((product) => (
+              {products.map((product ,index) => (
                 <div key={product.id}>
-                  <div className="flex justify-between bg-white h-40 m-0.5">
-                    <div>
-                      <img
-                        src={product.image}
-                        alt={product.title}
-                        className="h-20 w-auto m-8"
-                      />
+                <div
+                  className="flex justify-between bg-white m-0.5 hover:shadow-2xl"
+                  onMouseEnter={() => handleMouseEnter(index)}
+                  onMouseLeave={handleMouseLeave}
+                >
+                  <div className="mr-8">
+                    <img
+                      src={product.image}
+                      alt={product.title}
+                      className="h-20 w-auto m-8"
+                    />
+                  </div>
+                  <div className="p-4">
+                    <h3 className="text-sm text-gray-500">{product.category}</h3>
+                    <h4 className="text-md font-bold text-blue-600">
+                      {truncateTitle(product.title)}
+                    </h4>
+                    <div className="flex justify-between mt-4 text-lg">
+                      <div className="text-gray-600">
+                        <p>${product.price}</p>
+                      </div>
+                      <div
+                        className={`${
+                          isHovered[index] ? "bg-yellow-400" : "bg-gray-200"
+                        } text-center text-lg ml-8 p-2 rounded-full text-white`}
+                      >
+                        <MdAddShoppingCart />
+                      </div>
                     </div>
-                    <div className="p-4">
-                      <h3 className="text-sm text-gray-500">
-                        {product.category}
-                      </h3>
-                      <h4 className="text-md font-bold text-blue-600">
-                        {truncateTitle(product.title)}
-                      </h4>
-                      <p className="text-gray-700">${product.price}</p>
+                    <div
+                      className={`${
+                        isHovered[index] ? "flex justify-between" : "hidden"
+                      } 'flex justify-between mt-4 text-gray-300 border-t border-gray-300'`}
+                    >
+                      <div className="flex items-center">
+                        <span className="ml-2">Wishlist</span>
+                        <FaRegHeart />
+                      </div>
+                      <div className="flex items-center">
+                        <span className="ml-2">Compare</span>
+                        <FaCodeCompare />
+                      </div>
                     </div>
                   </div>
                 </div>
+              </div>
               ))}
             </Slider>
           </div>
